8213bbbac6b4a8062d140e0d7ecf3829cc2b0d22
AdamBigos <AdamBigos@users.noreply.github.com>
2017-03-10 18:08:57 +0100

    Add files via upload

--- js/Introduction_to_JavaScript.js
+++ js/Introduction_to_JavaScript.js
@@ -16,25 +16,10 @@ document.getElementById("violet").addEventListener('click', eventchangecolorviol
 
 document.getElementById("green").addEventListener('click', eventchangecolorgreen);
 
-
-
-
-
 var arraySelect = document.getElementsByTagName('select');
 
 var element = document.getElementById('content');
 
-function dropdownStyles(){
-    
-    var style = this.id;
-    var value = this.value;
-    
-    element.style[style] = value;
-    
-}
+function dropdownStyles() {var style = this.id; var value = this.value; element.style[style] = value; }
 
-for( var i = 0; i < arraySelect.length; i++ ){
-    
-    arraySelect[i].addEventListener( 'change', dropdownStyles );
-    
-}
+for (var i = 0; i < arraySelect.length; i++ ){arraySelect[i].addEventListener( 'change', dropdownStyles );}
