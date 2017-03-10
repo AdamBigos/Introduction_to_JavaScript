cc4ebfbaf43505023ce1e2d6349d894cbf3593c5
AdamBigos <AdamBigos@users.noreply.github.com>
2017-03-10 12:33:57 +0100

    Add files via upload

--- js/Introduction_to_JavaScript.js
+++ js/Introduction_to_JavaScript.js
@@ -15,3 +15,26 @@ document.getElementById("optionB").addEventListener('click', eventoptionB);
 document.getElementById("violet").addEventListener('click', eventchangecolorviolet);
 
 document.getElementById("green").addEventListener('click', eventchangecolorgreen);
+
+
+
+
+
+var arraySelect = document.getElementsByTagName('select');
+
+var element = document.getElementById('content');
+
+function dropdownStyles(){
+    
+    var style = this.id;
+    var value = this.value;
+    
+    element.style[style] = value;
+    
+}
+
+for( var i = 0; i < arraySelect.length; i++ ){
+    
+    arraySelect[i].addEventListener( 'change', dropdownStyles );
+    
+}
